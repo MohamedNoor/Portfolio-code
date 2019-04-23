e21490ca5839811d3c55da513ff7a3d9d76cb90a
MohamedNoor <Mohamed.Noor2106@gmail.com>
2019-04-23 20:13:49 +0100

    Intial commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Avatar from 'avataaars';
+//grid system is being imported from react mdl library
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <div className="animation-title">
+              <Avatar
+                avatarStyle='Transparent'
+                topType='ShortHairDreads01'
+                accessoriesType='Blank'
+                hairColor='Black'
+                facialHairType='BeardLight'
+                facialHairColor='Black'
+                clotheType='Hoodie'
+                clotheColor='Black'
+                eyeType='Default'
+                eyebrowType='Default'
+                mouthType='Smile'
+                skinColor='DarkBrown'
+              />
+          </div>
+            {/*title*/}
+            <div className="banner-text">
+              <h1 className="animation-title">Front-End Developer</h1>
+
+              <hr/>
+              {/*skills*/}
+              <p className="animation-skills">HTML | CSS | JavaScript | React | Python</p>
+
+              <div className="socila-links">
+              {/*Linked in*/}
+                <a href="" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin" aria-hidden="true"/>
+                </a>
+              {/*Github link*/}
+                <a href="https://github.com/MohamedNoor" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github" aria-hidden="true"/>
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Landing;
